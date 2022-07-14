3fb941696529ee7d4725f70a7bd23fa96e27f8e1
Matheus Fortini <matheusfortini7@gmail.com>
2022-07-14 19:39:02 -0300

    remove bug no css do botão delete de meus anúncios

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
   has_many_attached :photos
 
-  validates :title, :description, :address, :status, :iptu, :price, presence: true
+  validates :title, :description, :area, :address, :status, :iptu, :price, presence: true
   validates :bathrooms, :rooms, :suites, :parking_space, presence: true, numericality: { only_integer: true }
 end
